7f3a489838c2c49a27f52ae6b2d6b2bbf21ba6a1
zhangtaohua <zthvivid@163.com>
2018-05-20 10:40:15 +0800

    topic update api

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array'
+    'middleware' => ['serializer:array', 'bindings']
 ], function($api) {
 
     $api->group([
@@ -47,6 +47,9 @@ $api->version('v1', [
             // 发布话题
             $api->post('topics', 'TopicsController@store')
                 ->name('api.topics.store');
+            // 修改话题
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
         });
 
         // 短信验证码
