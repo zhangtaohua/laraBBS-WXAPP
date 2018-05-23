dcd08c9b11def2e08e2dae579c16cf78737e0464
zhangtaohua <zthvivid@163.com>
2018-05-23 14:13:55 +0800

    finial comit

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-// Registration Routes...
+// Registration Routes..
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
 
